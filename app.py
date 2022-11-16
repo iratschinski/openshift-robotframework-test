05ded7e045a5da807f1e5d54b98ee117d7642082
iratschinski <iratschinski@gmx.de>
2022-11-16 16:59:29 +0100

    changes / route

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def start():
+    start_tests()
+    return "Tests restarted TODO"
 
 @app.route('/reports')
 def send_index():
@@ -18,10 +19,13 @@ def send_report(path):
         path = "index.html"
     return send_from_directory('reports', path)
 
+def start_tests():
+    os.system("python test.py tests")
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
 
-    os.system("python test.py tests")
+    start_tests()
 
     app.run(port=port,host='0.0.0.0')
